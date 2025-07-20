b84ff23753a1068fe042a40ef527a588cc58f594
Adebay Timileyin <adebayotimileyin@gmail.com>
2025-07-20 10:11:08 -0600

    Add weather data and interface

--- project/include/IWeather.h
+++ project/include/IWeather.h
@@ -1,17 +1,20 @@
 #pragma once
+#include "ISensor.h"
 
 struct WeatherData
 {
-  double temp;
-  double presure;
-  double humidity;
+  double temp;     /* in Celsius */
+  double presure;  /* in millibar */
+  double humidity; /* in % */
   WeatherData(const double &t, const double &p, const double &h)
       : temp(t), presure(p), humidity(h) {}
 };
 
-class IWeather
+class IWeather : public ISensor
 {
 public:
+  virtual int Initialize() override { return 0; }
+  virtual void GetRawSensorData() override {}
   virtual WeatherData GetWeatherData() const
   {
     return WeatherData(18.0, 895.0, 56.0);
