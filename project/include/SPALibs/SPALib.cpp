e8650121e5f882dd095858b760c96ec108cba819
Adebay Timileyin <adebayotimileyin@gmail.com>
2025-07-20 10:10:40 -0600

    Add sun position algortithm

--- project/include/SPALibs/SPALib.cpp
+++ project/include/SPALibs/SPALib.cpp
@@ -1,1 +1,58 @@
 #include "SPALib.h"
+
+SPA_Output getSunPosition(const SPA_Input *input)
+{
+  SPA_Output outdata;
+  if (!input)
+  {
+    outdata.errCode = 1;
+    return outdata;
+  }
+  spa_data spa;
+  float min, sec;
+
+  // enter required input values into SPA structure
+  spa.year = input->dateTime.dt.year;
+  spa.month = input->dateTime.dt.month;
+  spa.day = input->dateTime.dt.day;
+  spa.hour = input->dateTime.tt.hour;
+  spa.minute = input->dateTime.tt.minute;
+  spa.second = input->dateTime.tt.second;
+  spa.timezone = input->dateTime.tt.timezone;
+  spa.delta_ut1 = input->dateTime.GetDelta_UT1();
+  spa.delta_t = input->dateTime.GetDelta_T();
+  spa.longitude = input->pos.Longitude;
+  spa.latitude = input->pos.Latitude;
+  spa.elevation = input->pos.Altitude;
+  spa.pressure = input->weather.presure;
+  spa.temperature = input->weather.temp;
+  /*
+  Left this as default until I learn
+  what it is and how to get it for diffrent location
+  */
+  spa.slope = 30;
+  spa.azm_rotation = -10;
+  spa.atmos_refract = 0.5667;
+  spa.function = SPA_ALL;
+
+  if (spa_calculate(&spa) != 0) // check for SPA errors
+  {
+    outdata.errCode = 2;
+    return outdata;
+  }
+
+  outdata.errCode = 0;
+  outdata.elevation = 90.0 - spa.zenith;
+  outdata.azimuth = spa.azimuth;
+  outdata.incidence = spa.incidence;
+
+  min = 60.0 * (spa.sunrise - (int)(spa.sunrise));
+  sec = 60.0 * (min - (int)min);
+  outdata.sunrise = Time(spa.sunrise, min, sec, input->dateTime.tt.timezone);
+
+  min = 60.0 * (spa.sunset - (int)(spa.sunset));
+  sec = 60.0 * (min - (int)min);
+  outdata.sunset = Time(spa.sunset, min, sec, input->dateTime.tt.timezone);
+
+  return outdata;
+}
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,5 +1,6 @@
 #include <math.h>
 #include "WMMLib.h"
+#include "SPALib.h"
 #include <iostream>
 
 int main()
@@ -9,10 +10,12 @@ int main()
   Point3f mag = imu.Get3DMagneticData();
   // Calculate the magnetic north direction (θ mag)
   double theta = atan(mag.Y / mag.X);
-  // Configure Input to get local north direction
   IDateTime dt;
   IGPSSensor gps;
   InData in;
+  IWeather weather;
+  SPA_Input spa_in(dt.GetDateTimeDate(), gps.GetPositionData(), weather.GetWeatherData());
+  // Configure Input to get local north direction
   in.decimalYear = dt.GetDecimalYear();
   in.pos = gps.GetPositionData();
   // Get local magnetic north direction (D)
@@ -22,13 +25,27 @@ int main()
     std::cerr << "An Error occurred: " << decl.errCode << " While trying to get local declinition" << std::endl;
     return 1;
   }
+  // https://www.suncalc.org/#/51.0108,-114.0646,18/2025.07.20/09:29/1/3
   // Calculate true north position releative to mag sensor (θ true = θ mag - D)
   auto trueNorth = theta - decl.magData.D;
-  // Print our result to console
-  std::cout << "Magnetic Sensor North: " << theta
-            << ", Local Declinition: " << decl.magData.D
-            << ", Local Declinition Error: " << decl.magDataErr.D
-            << ", True North: " << trueNorth << " due "
-            << (trueNorth > 0 ? "East" : "West") << std::endl;
+  // Get Sun position
+  auto spa_data = getSunPosition(&spa_in);
+  if (spa_data.errCode != 0)
+  {
+    std::cerr << "An Error occurred: " << spa_data.errCode << " While trying to get sun position" << std::endl;
+    return 1;
+  }
+  // Print result out to console
+  std::cout
+      << "Magnetic Sensor North [degrees]: " << theta
+      << "\nLocal Declinition [degrees]: " << decl.magData.D
+      << "\nLocal Declinition Error [degrees]: " << decl.magDataErr.D
+      << "\nTrue North [degrees]: " << trueNorth << " due " << (trueNorth > 0 ? "East" : "West")
+      << "\nAzimuth [degrees]: " << spa_data.azimuth
+      << "\nElevation [degrees]: " << spa_data.elevation
+      << "\nIncidence [degrees]: " << spa_data.incidence
+      << "\nSunrise: " << spa_data.sunrise.hour << ":" << spa_data.sunrise.minute << ":" << spa_data.sunrise.second << " localtime"
+      << "\nSunset: " << spa_data.sunset.hour << ":" << spa_data.sunset.minute << ":" << spa_data.sunset.second << " localtime"
+      << std::endl;
   return 0;
 }
