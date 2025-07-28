66fb1d37cb3ecd656ab1bc43d7e7ea95b5603465
Adebay Timileyin <adebayotimileyin@gmail.com>
2025-07-27 21:33:56 -0600

    add static test data for mag % gyro

--- project/include/IMUSensor.h
+++ project/include/IMUSensor.h
@@ -4,10 +4,12 @@
 
 struct IMUSensorData
 {
-  Point3f accel;
-  Point3f gyro;
-  Point3f mag;
+  Point3f accel; /* X, Y, Z (m/s^2) */
+  Point3f gyro;  /* Yaw, Pitch, Roll (degree) */
+  Point3f mag;   /* X, Y, Z (uT) */
   IMUSensorData() : accel(Point3f()), gyro(Point3f()), mag(Point3f()) {}
+  IMUSensorData(const Point3f &a, const Point3f &g, const Point3f &m)
+      : accel(a), gyro(g), mag(m) {}
 };
 
 class IMUSensor : public ISensor
@@ -15,8 +17,8 @@ class IMUSensor : public ISensor
 public:
   virtual int Initialize() override { return 0; }
   virtual void GetRawSensorData() override {}
-  virtual Point3f Get3DAccelerometerData() const { return Point3f(); }
-  virtual Point3f Get3DGyroscopeData() const { return Point3f(); }
+  virtual Point3f Get3DAccelerometerData() const { return Point3f(-9.250, 0.307, 3.224); }
+  virtual Point3f Get3DGyroscopeData() const { return Point3f(-101.4, -1.708, 70.732); }
   virtual Point3f Get3DMagneticData() const { return Point3f(-47.5, -21.1, -53.7); }
-  virtual IMUSensorData GetIMUSensorData() const { return IMUSensorData(); }
+  virtual IMUSensorData GetIMUSensorData() const { return IMUSensorData(Get3DAccelerometerData(), Get3DGyroscopeData(), Get3DMagneticData()); }
 };
